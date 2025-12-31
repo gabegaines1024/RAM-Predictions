8fdecaa8caeda815c27144798980e6ece8055aa8
Gabe <gabegaines1024@gmail.com>
2025-12-31 10:16:29 -0600

    data is pretty skewed, going to using logarithmic arithmetic on it

---
+++ preprocessing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/memory.csv')
+
+"""
+name                   object
+price                 float64
+speed                  object
+modules                object
+price_per_gb          float64
+color                  object
+first_word_latency    float64
+cas_latency           float64
+dtype: object
+name                     0
+price                 2783
+speed                    2
+modules                  0
+price_per_gb          2837
+color                  505
+first_word_latency      95
+cas_latency             33
+"""
+
